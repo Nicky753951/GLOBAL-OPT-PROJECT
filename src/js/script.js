4ffb8e8c6f1e8a1177f750b55df4834137d5c248
Nick <sytnyk.m@ukr.net>
2021-05-15 12:38:21 +0300

    third comment

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const hamburger = document.querySelector('.fixed-hamburger__hamburger'),
+const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close'),
       menuLink = document.querySelectorAll('.menu__link'),
@@ -21,8 +21,8 @@ const hamburger = document.querySelector('.fixed-hamburger__hamburger'),
         item.addEventListener('click', () => {
             menu.classList.remove('active');
             hamburger.classList.remove("spanX");
-        })
-    })
+        });
+    });
 	// на майбутє,якщо буду робити фіксоване меню для десктопа
     document.addEventListener("keydown", (e) => {
 	    if (e.code === 'Escape') {
@@ -42,4 +42,57 @@ const hamburger = document.querySelector('.fixed-hamburger__hamburger'),
 };
 toggleSlide('.btn_forward');
 toggleSlide('.btn_back');
+
+  $('.carousel').slick({
+    pauseOnDotsHover:true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+     responsive: [
+    {
+      breakpoint: 993,
+      settings: {
+        arrows: false,
+        dots:true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 888,
+      settings: {
+        arrows: false,
+        dots:true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        dots:true
+      }
+    }
+  ]
+  });
+
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+  $("a[href='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+  
 });
