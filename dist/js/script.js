e01f65e4a12b91110e5b663d6ac9d02802b5b244
Nick <sytnyk.m@ukr.net>
2021-10-09 21:13:23 +0300

    fixing

--- dist/js/script.js
+++ dist/js/script.js
@@ -169,6 +169,19 @@ function initMap(){
       icon: image
     });
 
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener ("click", function(e) {
+            e.preventDefault();
+            const blockId = anchor.getAttribute('href');
+            document.querySelector('' + blockId).scrollIntoView({
+                behavior:"smooth",
+                block:"start"
+            })
+        })
+    }
+
     // const InfoWindow = new google.maps.InfoWindow({
     //   content: "<div class='address'> <p class='address__city'>г. Москва</p> <p class='address__street'>ул. Садовническая, дом 5,офис 4-6, 700 от м. Новокузнецкая</p> <p class='address__tel'>Тел: +7 (926) 423 01 00</p> <p class='address__email'>info@glopt.ru</p></div>"
     // });
